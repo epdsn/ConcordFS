fd3a0dd5688a50ba5bf39df97b5076beab59eaaa
Eric Perez <ibestudios@gmail.com>
2024-03-07 21:55:45 -0700

    log configuration

--- ConcordeFS.py
+++ ConcordeFS.py
@@ -4,17 +4,8 @@ import logging
 
 # Configure logging
 logging.basicConfig(filename='debug.log', 
-                    level=logging.INFO,  
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    datefmt='%Y-%m-%d %I:%M:%S %p')
-
-# Log some messages
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
-
+                    level=logging.DEBUG, datefmt='%Y-%m-%d %I:%M:%S %p', 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Initialize Pygame
 pygame.init()
